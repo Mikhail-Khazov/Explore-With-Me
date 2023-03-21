bb8ccb37b04dc21033de341f49fcd7278251b9cc
Mikhail_Khazov <mkh1991@yandex.ru>
2023-03-22 02:14:00 +0300

    statistic service
    statistic service
    
    statistic service

--- statistics-explore-with-me/stat-service/src/main/java/ru/practicum/ewm/hit/storage/EndpointsHitStorage.java
+++ statistics-explore-with-me/stat-service/src/main/java/ru/practicum/ewm/hit/storage/EndpointsHitStorage.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.ewm.hit.model.EndpointsHit;
 import ru.practicum.ewm.hit.model.StatsResponse;
 
